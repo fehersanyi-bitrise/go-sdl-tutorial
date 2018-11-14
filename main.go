3240659358fa0768e6103d7947a6f5433199726e
fehersanyi-bitrise <sandor.feher@bitrise.io>
2018-11-14 18:49:12 +0100

    window shows properly, but the gradient does not draw out :(

--- main.go
+++ main.go
@@ -23,12 +23,20 @@ func setPixel(x, y int, c color, pixels []byte) {
 		pixels[index+1] = c.g
 		pixels[index+2] = c.b
 	}
-	return
+
 }
 
 func main() {
 
-	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, int32(windowWidth), int32(windowHeight), sdl.WINDOW_SHOWN)
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		int32(windowWidth), int32(windowHeight), sdl.WINDOW_SHOWN)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -41,18 +49,32 @@ func main() {
 		return
 	}
 	defer renderer.Destroy()
+
 	tex, err := renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STREAMING, int32(windowWidth), int32(windowHeight))
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer tex.Destroy()
-	//pixels := make([]byte, windowWidth*windowHeight*4)
 
-	//for i := range pixels {
-	//setPixel()
-	//print(i)
-	//}
-	sdl.Delay(20000)
+	pixels := make([]byte, windowWidth*windowHeight*4)
 
+	for y := 0; y < windowHeight; y++ {
+		for x := 0; x < windowWidth; x++ {
+			setPixel(x, y, color{255, 255, 0}, pixels)
+		}
+	}
+
+	tex.Update(nil, pixels, windowWidth*4)
+	renderer.Copy(tex, nil, nil)
+	renderer.Present()
+
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				return
+			}
+		}
+	}
 }
